e75c3bb14767992a48d37df22bf3c6e1b1894457
Gustavo <gus.inacio@hotmail.com>
2022-07-16 21:03:18 -0300

    feat: endpoint to get takeblip's repositories data

--- api/app.ts
+++ api/app.ts
@@ -1,4 +1,18 @@
+import axios from 'axios';
 import express from 'express';
+
+interface Repository {
+  id: number;
+  name: string;
+  full_name: string;
+  description: string;
+  language: string;
+  owner: {
+    id: number;
+    login: string;
+    avatar_url: string;
+  },
+}
 
 export default class App {
   public app: express.Express
@@ -14,9 +28,14 @@ export default class App {
   private router() {
     this.app.get('/', async (_req, res) => {
       try {
-        //
+        const { data } = await axios.get<Repository[]>(`${this.API_URL}/users/takenet/repos?&sort=created&direction=asc`);
+
+        const response = data.filter((repo) => repo.language === 'C#').slice(0, 5);
+
+        return res.status(200).json(response);
       } catch (error: any) {
         console.log(error.message);
+        return res.status(200).json({ message: 'Internal server error' });
       }
     });
   }
